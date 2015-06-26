9948a98ed15af9468a0aafaa37df0d828846f7f0
cameronjacoby <cameron.jacoby@gmail.com>
2015-06-25 22:11:45 -0700

    add functions interview prep solutions

---
+++ w1_d4_0_intro_functions/solutions/swap.js
@@ -0,0 +1,13 @@
+var x = "a";
+var y = "b";
+
+// check values of x and y
+console.log("x: " + x + ", y: " + y);
+
+// swap x and y
+var temp = x;
+x = y;
+y = temp;
+
+// check values of x and y again
+console.log("x: " + x + ", y: " + y);
