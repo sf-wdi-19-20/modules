6385741caa1477d280b253dd531868ef5328822c
cameronjacoby <cameron.jacoby@gmail.com>
2015-07-05 14:36:08 -0700

    start spotify app solution

---
+++ w3_d1_2_apis_continued/spotify_app/solution/script.js
@@ -0,0 +1,46 @@
+$(function() {
+
+  // form to search spotify API
+  var $spotifySearch = $('#spotify-search');
+
+  // element to hold results from spotify API
+  var $results = $('#results');
+
+  // track (song) template
+  var trackTemplate = _.template($('#track-template').html());
+
+  // submit form to search spotify API
+  $spotifySearch.on('submit', function(event) {
+    event.preventDefault();
+
+    $results.empty();
+
+    // save form data to variable
+    var searchTrack = $('#track').val();
+
+    // spotify search URL
+    var searchUrl = 'https://api.spotify.com/v1/search?type=track&q=' + searchTrack;
+
+    // use AJAX to call spotify API
+    $.get(searchUrl, function(data) {
+      console.log(data);
+      var trackResults = data.tracks.items;
+      console.log(trackResults);
+
+      _.each(trackResults, function(result, index) {
+        var templateData = {
+          albumArt: result.album.images.length > 0 ? result.album.images[0].url : null,
+          artist: result.artists[0].name,
+          name: result.name
+        };
+        var $trackResult = $(trackTemplate(templateData));
+        $results.append($trackResult);
+      });
+    });
+
+    // reset the form
+    $spotifySearch[0].reset();
+    $('#track').focus();
+  });
+
+});
