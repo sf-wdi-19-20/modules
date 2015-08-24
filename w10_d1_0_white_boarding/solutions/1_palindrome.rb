6e5445d883960223bd6bfafebda4e04e38278c87
cameronjacoby <cameron.jacoby@gmail.com>
2015-08-23 20:51:12 -0700

    palindrome solutions

--- w10_d1_0_white_boarding/solutions/1_palindrome.rb
+++ w10_d1_0_white_boarding/solutions/1_palindrome.rb
@@ -1,1 +1,19 @@
 # Problem 1 - Palindrome
+
+def is_palindrome(str)
+  # simple solution
+  str = str.downcase
+
+  # removing spaces and punctuation
+  # str = str.downcase.gsub(/(\s+|\W+)/, '');
+
+  i = 0
+  while i < str.length
+    if str[i] != str[str.length - i - 1]
+      return false
+    end
+    i += 1
+  end
+
+  return true
+end
